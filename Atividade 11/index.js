bf483d9954f58b6a695443420c81c3ae0aaf3bba
NadyCarboni <nady.n.m.c@gmail.com>
2021-10-19 10:25:53 -0200

    Atividade 11

--- Atividade 11/index.js
+++ Atividade 11/index.js
@@ -7,8 +7,7 @@ function Retangulo(x, y){
    this.y = y;
     // método público
     this.calcArea = function(){ 
-        return x * y;
-        
+        return x * y; 
    }
 }
 
@@ -21,6 +20,7 @@ console.log(retangulo.calcArea());
 document.write('O retângulo tem as medidas x = ' + retangulo.x + ' y = ' + retangulo.y + ' e sua área é igual a: ' + retangulo.calcArea() + '\n\n\n');
 
 
+
 /*----------EXERCICIO 2----------*/
 
 function Conta(nomeCorrent, banco, nConta, saldo) {
@@ -29,30 +29,99 @@ function Conta(nomeCorrent, banco, nConta, saldo) {
     this.nConta = nConta;
     this.saldo = saldo;
 
+    this.setnomeCorrent = function(nome) {
+        nomeCorrent = nome;
+    }
+    this.setBanco = function(nomeBanco) {
+        banco = nomeBanco;
+    }
+    this.setNConta = function(numConta) {
+        nConta = numConta;
+    }
+    this.setSaldo = function(valor) {
+        saldo = valor;
+    }
+    this.getnomeCorrent = function() {
+       return nomeCorrent;
+    }
+    this.getBanco = function() {
+        return banco;
+    }
+    this.getNConta = function() {
+       return nConta;
+    }
+    this.getSaldo = function() {
+        return saldo;
+    }
+
 }
 
 function ContaCorrente(saldoEspecial) {
-    this.nomeCorrent = nomeCorrent;
-    this.banco = banco;
-    this.nConta = nConta;
-    this.saldo = saldo;
+    
 
     this.saldoEspecial = saldoEspecial;
+    this.setSaldoEspecial = function(valor) {
+        saldoEspecial = valor;
+    }
+    this.getSaldoEspecial = function() {
+        return saldoEspecial;
+    }
 }
 
 function ContaPoupanca(juros, dataVenc) {
-    this.nomeCorrent = nomeCorrent;
-    this.banco = banco;
-    this.nConta = nConta;
-    this.saldo = saldo;
-    
+   
+
     this.juros = juros;
     this.dataVenc = dataVenc;
-
+    this.setJuros = function(valor) {
+        juros = valor;
+    }
+    this.getJuros = function() {
+        return juros;
+    }
+    this.setDataVenc= function(data) {
+        dataVenc = data;
+    }
+    this.getDataVenc = function() {
+        return dataVenc;
+    }
 }
 
-ContaCorrente.prototype = new Conta();
+
+
 ContaPoupanca.prototype = new Conta();
+var contaPoup = new ContaPoupanca();
+
+contaPoup.setnomeCorrent('Alfredo');
+contaPoup.setBanco('Banco legal');
+contaPoup.setNConta('000');
+contaPoup.setSaldo('50');
+contaPoup.setDataVenc('22/07/2002');
+contaPoup.setJuros('1%');
 
 
 
+ContaCorrente.prototype = new Conta();
+var contaCor = new ContaCorrente();
+
+contaCor.setnomeCorrent('Rodolfo');
+contaCor.setBanco('Banco legal');
+contaCor.setNConta('001');
+contaCor.setSaldo('40');
+contaCor.setSaldoEspecial('22/07/2002');
+
+
+var texto = 'Conta poupança:<br>Nome: ' + contaPoup.getNConta() + '<br> Banco: ' + contaPoup.getBanco() 
++ '<br>Número conta: ' + contaPoup.getNConta() + '<br>Saldo: ' +  contaPoup.getSaldo() + '<br>Juros: ' 
++ contaPoup.getJuros() + '<br>Data Vencimento: ' + contaPoup.getDataVenc() + '<br><br><br>';
+
+
+var texto2 = 'Conta Corrente:<br>Nome: ' + contaCor.getNConta() + '<br> Banco: ' + contaCor.getBanco() 
++ '<br>Número conta: ' + contaCor.getNConta() + '<br>Saldo: ' +  contaCor.getSaldo() + '<br>Saldo especial: ' 
++ contaCor.getSaldoEspecial() + '<br><br><br>';
+
+
+
+document.getElementById('text').innerHTML = `${texto}` ; 
+
+document.getElementById('text2').innerHTML = `${texto2}` ; 
